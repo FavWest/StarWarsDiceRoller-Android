442b7957b334f5e7f2e77ba1c96cb1b10b461a07
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-06 16:10:35 -0700

    roll button displays result message

--- app/src/main/java/com/favwest/starwarsdiceroller/MainActivity.kt
+++ app/src/main/java/com/favwest/starwarsdiceroller/MainActivity.kt
@@ -3,14 +3,24 @@ package com.favwest.starwarsdiceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.VISIBLE
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        val resultsTextView: TextView = findViewById(R.id.results)
+
+        rollButton.setOnClickListener{
+            resultsTextView.text = "5 successes!"
+            resultsTextView.visibility = VISIBLE
+        }
     }
-//    private val results: TextView = findViewById<TextView>(R.id.results)
 //
 //    fun roll() {
 //        results.visibility= View.VISIBLE
