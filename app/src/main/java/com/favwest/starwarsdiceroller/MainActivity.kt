1e01073b6988d6b8116242a97e8fb651e842f24a
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-06 11:52:45 -0700

    create layout for app

---
+++ app/src/main/java/com/favwest/starwarsdiceroller/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.favwest.starwarsdiceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+//    private val results: TextView = findViewById<TextView>(R.id.results)
+//
+//    fun roll() {
+//        results.visibility= View.VISIBLE
+}
