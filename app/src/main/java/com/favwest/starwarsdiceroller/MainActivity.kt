9df333d303688f855aa3086f3b1a6f9d3baabaaf
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-07 11:59:05 -0700

    add rollGreen function

--- app/src/main/java/com/favwest/starwarsdiceroller/MainActivity.kt
+++ app/src/main/java/com/favwest/starwarsdiceroller/MainActivity.kt
@@ -15,13 +15,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         val resultsTextView: TextView = findViewById(R.id.results)
+        val numGreen: TextView = findViewById(R.id.num_green)
 
-        rollButton.setOnClickListener{
-            resultsTextView.text = "5 successes!"
+        rollButton.setOnClickListener {
+            var greenDice = numGreen.text.toString().toInt()
+            Toast.makeText(this, greenDice.toString(), Toast.LENGTH_LONG).show()
+            val resultArray=rollGreen(greenDice + 1)
+            resultsTextView.text = "${resultArray[0]} success, ${resultArray[1]} advantage"
             resultsTextView.visibility = VISIBLE
         }
     }
-//
-//    fun roll() {
-//        results.visibility= View.VISIBLE
+
+    private fun rollGreen(num:Int): Array<Int> {
+        var success = 0
+        var advantage = 0
+        for(dice in 1..num){
+            when((1..8).random()){
+                2, 3 -> success ++
+                4 -> success += 2
+                5, 6 -> advantage ++
+                7 -> {
+                    success ++
+                    advantage ++
+                }
+                8 -> advantage += 2
+            }
+        }
+        return arrayOf(success,advantage,0,0,0,0)}
 }
